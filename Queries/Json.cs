1a656cdb1a9c359a50537abd436e3ef324853d82
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2024-10-13 16:42:02 +0300

    add raw string json example

--- Queries/Json.cs
+++ Queries/Json.cs
@@ -4,7 +4,31 @@ public class Json
 {
     private static void RawStrings()
     {
+        //language=json
+        var raw1 = """{"status": "success", "code": 200}""";
 
+        //language=json
+        var raw2 = """
+            {
+              "person": {
+                "name": "Emma",
+                "age": 28,
+                "hobbies": ["reading", "swimming"]
+              }
+            }
+            """;
 
+        var raw3 = string.Empty;
+        //language=json
+        raw3 = """
+            {
+              "product": {
+                "id": "p123",
+                "name": "Laptop",
+                "price": 999.99,
+                "inStock": true
+              }
+            }
+            """;
     }
 }
