c5216c54ad81652564030e3b3e065d5d8cbe639e
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2024-10-13 15:53:58 +0300

    add quoted and verbatim sql string examples

--- Queries/Sql.cs
+++ Queries/Sql.cs
@@ -2,8 +2,37 @@ namespace Queries;
 
 public class Sql
 {
-	public Sql()
+	private static void QuotedStrings()
 	{
+		//language=sql
+		var string1 = "select * from users where id = 5;";
+
+		//language=sql
+		string string2 = "select * from users where id = 5";
+
+		//language=sql
+		const string string3 = "select * from users where id = 5;";
+
+		var string4 = string.Empty;
+		//language=sql
+		string4 = "select * from users where id = 5;";
+	}
+
+	private static void VerbatimStrings()
+	{
+		//language=sql
+		var string1 = @"select * from users where id = 5;";
+
+		//language=sql
+		string string2 = @"select * from users where id = 5";
+
+		//language=sql
+		const string string3 = @"select * from users where id = 5;";
+
+		var string4 = string.Empty;
+		//language=sql
+		string4 = @"select * from users where id = 5;";
+	}
 
 	}
 }
