60cdc96da962c8b7f6b6dff44492c59f264d86a8
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2024-10-13 16:27:06 +0300

    add raw sql string example

--- Queries/Sql.cs
+++ Queries/Sql.cs
@@ -34,5 +34,26 @@ public class Sql
 		string4 = @"select * from users where id = 5;";
 	}
 
+	private static void RawStrings()
+	{
+		//language=sql
+		var raw1 = """select * from users""";
+
+		//language=sql
+		var raw2 = """
+			SELECT u.Id, u.Name, u.Email, COUNT(o.Id) AS OrderCount
+			FROM users AS u
+			INNER JOIN orders AS o ON u.Id = o.OwnerId
+			GROUP BY u.Id, u.Name, u.Email;
+			""";
+
+		var raw3 = string.Empty;
+		//language=sql
+		raw3 = """
+			SELECT u.Id, u.Name, u.Email, COUNT(o.Id) AS OrderCount
+			FROM users AS u
+			INNER JOIN orders AS o ON u.Id = o.OwnerId
+			GROUP BY u.Id, u.Name, u.Email;
+			""";
 	}
 }
